a4e279f15fb7fd0987f45458c1a781cf03b85950
EduardoA08 <54415128+EduardoA08@users.noreply.github.com>
2019-09-13 12:30:47 -0600

    Add files via upload

---
+++ labo2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+/*
+void mcd (int mayor=0, int menor=0){
+    
+    int residuo=0;
+    
+    residuo= mayor%menor;
+    
+    if(residuo==0){
+     cout << menor <<endl;   
+}
+    else{
+        int aux=0;
+        aux = menor;
+        mcd(aux,residuo);
+    }
+}
+
+
+int main(){
+    
+    int mayor = 0, menor = 0, residuo=0; 
+    
+    cout<<"Ingrese el numero mayor: ";
+    cin>>mayor;
+    
+    cout<<"Ingrese el numero menor: ";
+    cin>>menor;
+    
+    mcd(mayor,menor);
+    
+    return 0;
+}
+*/
+
+void cant(int num=0){
+    
+    
+    
+}
+
+
+
+
+int main(){
+    
+    int num=0;
+    
+    cout<<"Digite un numero: ";
+    cin>>num;
+    
+    cant(num);
+    
+    cout<<"La cantidad de digitos es: "<<num;
+    
+    return 0;
+}
